02735bbd08f8292b6f568427dcd7a71b3b93f3d0
Slava Ilyin <savok.ultrasound@gmail.com>
2023-01-29 14:24:22 +0300

    Add gitlessons_MyFirstApp

---
+++ Git_Lessons/MyFirstApp.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+//import java.nio.charset.Charset;
+
+public class MyFirstApp {   
+    public static void main(String[] args) {
+        System.out.println("Написано однажды, работает везде");
+        Scanner console = new Scanner (System.in, "cp866");
+        System.out.print("Введите свое имя: ");
+        System.out.print(console.nextLine());
+        System.out.print(" - Так вас зовут ");
+    //public static void main(String[] args) {
+        //System.out.println(Charset.defaultCharset().displayName());
+    }
+}
