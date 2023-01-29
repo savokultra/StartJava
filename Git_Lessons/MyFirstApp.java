665633bfdf86b73c1fc7dadedc554f4d7fd843af
Slava Ilyin <savok.ultrasound@gmail.com>
2023-01-29 15:01:08 +0300

    solve_merge

--- Git_Lessons/MyFirstApp.java
+++ Git_Lessons/MyFirstApp.java
@@ -8,6 +8,7 @@ public class MyFirstApp {
         Scanner console = new Scanner (System.in, "cp866");
         System.out.print("Введите свое имя: ");
         System.out.print(console.nextLine());
+<<<<<<< HEAD
         System.out.print(" - Так вас зовут ");
         System.out.print("Изменил коммит в в ветке main");
     //public static void main(String[] args) {
