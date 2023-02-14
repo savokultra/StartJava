939287e86e6622b7ef8347822d8a47482d0eacdb
Slava Ilyin <savok.ultrasound@gmail.com>
2023-02-14 21:34:23 +0300

    CyclesTheme_2

--- Lesson_1/CyclesTheme.java
+++ Lesson_1/CyclesTheme.java
@@ -1,6 +1,6 @@
 public class CyclesTheme {
 
-    public static viod main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Задание 1: Подсчет суммы четных и нечетных чисел;");
         int i = -10;
         int sumEven = 0;
@@ -17,49 +17,66 @@ public class CyclesTheme {
                 ", а нечетных = " + sumOdd);
 
         System.out.println("\nЗадание 2: Вывод чисел в интервале (min и max) в порядке убывания;");
-        int point1 = 10;
-        int point2 = 5;
-        int point3 = -1;
+        int value1 = 10;
+        int value2 = 5;
+        int value3 = -1;
         int max = 0;
         int min = 0;
-        int i = 0;
-        if (point1 > point2 && point1 > point3) {
-            max = point1;
+        int j = 0;
+        if (value1 > value2 && value1 > value3) {
+            max = value1;
         } else {
-            min = point1;
+            min = value1;
         }
-        if (point2 > point1 && point2 > point3) {
-            max = point2;
+        if (value2 > value1 && value2 > value3) {
+            max = value2;
         } else {
-            min = point2;
+            min = value2;
         }
-        if (point3 > point1 && point3 > point2) {
-            max = point3;
+        if (value3 > value1 && value3 > value2) {
+            max = value3;
         } else {
-            min = point3;
+            min = value3;
         }
-        for (i = --max; i > min; --i) {
-            System.out.print(i);
+        for (j = --max; j > min; --j) {
+            System.out.print(j);
         }
 
-        System.out.println("\nЗадание 3: Вывод реверсивного числа и суммы его цифр;");
+        System.out.println("\n\nЗадание 3: Вывод реверсивного числа и суммы его цифр;");
         int number = 1234;
-        int i = 0;
-        int thousend = 0;
+        int k = 0;
+        int thousand = 0;
         int hundred = 0;
-        int ten = 0;cd
+        int ten = 0;
         int one = 0;
-        while (i < number) {
-            i++;
-            thousend = i / 1000;
-            hundred = (i - 1000) / 100;
-            ten = (i % 100) / 10;
-            one = i % 10;
+        while (k < number) {
+            k++;
+            thousand = k / 1000;
+            hundred = (k - 1000) / 100;
+            ten = (k % 100) / 10;
+            one = k % 10;
         }
-        System.out.println("thousend " + thousend);
-        System.out.println("hundred " + hundred);
-        System.out.println("ten " + ten);
-        System.out.println("one " + one);
-        System.out.println("i " + i);
+        int sum = one + ten + hundred + thousand;
+        System.out.printf("Исходное число в обратном порядке: %d%d%d%d", one, ten, hundred, 
+                thousand);
+        System.out.println("\nСумма цифр: " + sum);
+
+        System.out.println("\nЗадание 4: Вывод чисел на консоль в несколько строк;");
+        //полуинтервал [1, 24) число 24 не включаем в полуинтервал
+        int l = 1;
+        for (; l < 11; l+= 2) {
+            System.out.printf("%3d", l);
+        }
+        System.out.println("");
+        for (; l < 21; l+= 2) {
+            System.out.printf("%3d", l);
+        }
+        System.out.println("");
+        for (; l < 24; l+= 2) {
+            System.out.printf("%3d", l);
+        }
+        while (l < 31) {
+            System.out.printf("%3d", 0, l+= 2);
+        }
     }
 }
