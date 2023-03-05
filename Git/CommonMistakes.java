18129770d404949c5450fc72574ea824fbb548b1
Slava Ilyin <savok.ultrasound@gmail.com>
2023-03-05 17:26:47 +0300

    commit8

--- Git/CommonMistakes.java
+++ Git/CommonMistakes.java
@@ -3,5 +3,6 @@ public class CommonMistakes {
     public static void main(String[] args) {
         System.out.println("Урок 6: Распространенные ошибки;");
         System.out.println("Урок 7: Распространенные ошибки;");
+        System.out.println("Урок 8: Распространенные ошибки;");
     }
 }
