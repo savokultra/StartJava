4820da49306918ede934713cd766abfcdc7096fa
Slava Ilyin <savok.ultrasound@gmail.com>
2023-03-05 13:26:33 +0300

    GitFastStart

---
+++ Git/GitFastStart.java
@@ -0,0 +1,7 @@
+public class GitFastStart {
+
+    public static void main(String[] args) {
+        System.out.println("Задание 1: Clone, commit, push, pull");
+        System.out.println("Задание 1: Clone, commit, push, pull");
+    }
+}
