bcf92165acc933dc82c251be367147be1a90caf9
Slava Ilyin <savok.ultrasound@gmail.com>
2023-03-05 16:22:02 +0300

    Super goodbye

--- Git/GoodByeWorld.java
+++ Git/GoodByeWorld.java
@@ -3,5 +3,6 @@ public class GoodByeWorld {
     public static void main(String[] args) {
         System.out.println("Задание 4: GoodByeWorld");
         System.out.println("Задание 4: NewGoodByeWorld");
+        System.out.println("Задание 5: SuperGoodByeWorld");
     }
 }
