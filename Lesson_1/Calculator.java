a8333f30c5b052757c4e4e41678914cbd18978b6
Slava Ilyin <savok.ultrasound@gmail.com>
2023-04-02 23:15:53 +0300

    calc_maybe_good

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,8 +1,27 @@
 public class Calculator {
 
     public static void main (String [] args) {
-        int number = 1;
-        int number1 = 2;
-        char sign = '*';
+        int number = 4;
+        int number1 = 3;
+        char sign = '^';
+        int numberCopy = number;
+        if (sign == '+') {
+            number+= number1;
+        } else if (sign == '-') {
+            number-= number1;
+        } else if (sign == '*') {
+            number*=  number1;
+        } else if (sign == '/') {
+            number/= number1;
+        } else if (sign == '%') {
+            number%=  number1;
+        } else if (sign == '^') {
+            int count = number1;
+            while (count > 1) {
+                number*= number;
+                count--;
+            }
+        }
+        System.out.println(numberCopy + " " + sign + " " + number1 + " = " + number);
     }
 }
