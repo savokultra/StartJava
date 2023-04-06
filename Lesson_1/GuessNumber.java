d34663bf577d92ac692f6b2efc058938a5324491
Slava Ilyin <savok.ultrasound@gmail.com>
2023-04-06 21:50:11 +0300

    GuessNumber change mistakes

--- Lesson_1/GuessNumber.java
+++ Lesson_1/GuessNumber.java
@@ -1,20 +1,20 @@
 public class GuessNumber {
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         //(0, 100] a < x <= b
-        int computerNumber = 53;
-        int playerNumber = 10;
-        while (playerNumber != computerNumber) {
-            if (playerNumber <= computerNumber) {
-                System.out.println("число " + playerNumber + " меньше того, что загадал компьютер");
-                playerNumber++;
-            } else if (playerNumber >= computerNumber) {
+        int targetNumber = 53;
+        int playerNumber = 51;
+        while (true) {
+            if (playerNumber == targetNumber) {
+                System.out.println("Вы победили!");
+                break;
+            } else if (playerNumber > targetNumber) {
                 System.out.println("число " + playerNumber + " больше того, что загадал компьютер");
                 playerNumber--;
+            } else {
+                System.out.println("число " + playerNumber + " меньше того, что загадал компьютер");
+                playerNumber++;
             }
-        }
-        if (playerNumber == computerNumber) {
-            System.out.println("Вы победили!");
         }
     }
 }
