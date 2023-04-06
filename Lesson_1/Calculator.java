ca6ca025eec71500d5a7e4447d3b2fc047ea6b82
Slava Ilyin <savok.ultrasound@gmail.com>
2023-04-06 21:49:23 +0300

    format

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,6 +1,6 @@
 public class Calculator {
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         int number = 4;
         int number1 = 3;
         char sign = '^';
