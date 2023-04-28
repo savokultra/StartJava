3980b345685113506651e87a0382008ced183d8c
Slava <121571166+savokultra@users.noreply.github.com>
2023-04-28 13:16:31 +0300

    Create Person.java

---
+++ Lesson_2/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+    
+    String sex = "male";
+    String name = "Petya";
+    float height = 1.8f;
+    float weight = 62.5f;
+    int age = 200;
+    
+    void move() {
+        System.out.println("moving");
+    }
+    
+    void run() {
+        System.out.println("runing");
+    }
+    
+    String talk() {
+        return "talking";
+    }
+    
+    boolean learnJava() {
+        return true;
+    }
+}
