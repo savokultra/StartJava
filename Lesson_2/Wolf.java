5409b514a76de8d25c95ada7172d417a0223bb02
Slava Ilyin <savok.ultrasound@gmail.com>
2023-05-22 20:30:42 +0300

    wolfTest getters and setters update

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,10 +1,10 @@
 public class Wolf {
 
     private String name;
-    //private String color;
-    //private float weight;
-    //private int age;
-    //private boolean male;
+    private String color;
+    private float weight;
+    private int age;
+    private boolean male;
 
     public void move() {
         System.out.println("Идёт");
@@ -36,6 +36,43 @@ public class Wolf {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public boolean isMale() {
+        return male;
+    }
+
+    public void setMale(boolean male) {
+        this.male = male;
+    }
+
 }
 
 
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -3,15 +3,15 @@ public class WolfTest {
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
         wolfOne.setName("Боря");
-        //wolfOne.color = "Серый";
-        //wolfOne.weight = 62.5f;
-        //wolfOne.age = 3;
-        //wolfOne.male = true;
+        wolfOne.setColor("Серый");
+        wolfOne.setWeight(62.5f);
+        wolfOne.setAge(8);
+        wolfOne.setMale(true);
         System.out.println("Волка зовут: " + wolfOne.getName());
-        //System.out.println("Цвет волка: " + wolfOne.color);
-        //System.out.println("Вес волка: " + wolfOne.weight + " кг");
-        //System.out.println("Возраст волка: " + wolfOne.age + " лет");
-        //System.out.println("Пол волка самец: " + wolfOne.male);
+        System.out.println("Цвет волка: " + wolfOne.getColor());
+        System.out.println("Вес волка: " + wolfOne.getWeight() + " кг");
+        System.out.println("Возраст волка: " + wolfOne.getAge() + " лет");
+        System.out.println("Пол волка самец: " + wolfOne.isMale());
         wolfOne.move();
         wolfOne.sit();
         wolfOne.run();
