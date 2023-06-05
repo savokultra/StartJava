d3a401cd93ee0e0ad6f6fb9d5ea3a20bf465294e
Slava Ilyin <savok.ultrasound@gmail.com>
2023-06-05 20:52:04 +0300

    Jaegers

---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,86 @@
+public class Jaeger {
+
+    private String modelName;
+    private String mark;
+    private String origin;
+    private float height;
+    private float weight;
+    private int speed;
+    private int strength;
+    private int armor;
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    private float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    private float getWeight() {
+        return height;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    private int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    private int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    private int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    boolean move() {
+        System.out.println("Робот находится в движении");
+        return true;
+    }
+
+    int speed() {
+        System.out.println("скорость робота " + getSpeed());
+        return speed;
+    }
+
+}
---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,9 @@
+public class JaegerTest {
+
+    public static void main(String[] args) {
+        Jaeger flyJaeger = new Jaeger();
+        flyJaeger.setSpeed(20);
+        flyJaeger.move();
+        flyJaeger.speed();
+    }
+}
