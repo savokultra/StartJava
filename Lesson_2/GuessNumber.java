686b39d10d504e6c531331f801efc58ab6bbad5c
Slava <121571166+savokultra@users.noreply.github.com>
2023-06-27 13:29:09 +0300

    Update GuessNumber.java

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -26,6 +26,22 @@ public class GuessNumber {
         int number = sc.nextInt();
         Player player = new Player(number);
     }
+    
+    public static void playerInsert() { 
+        System.out.println("Введите имя игрока: ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        Player player = new Player(name);
+        System.out.println("имя игрока: " + player.getName());
+        System.out.println("Введите имя второго игрока: ");
+        String name1 = sc.nextLine();
+        Player player1 = new Player(name1);
+        System.out.println("имя игрока 2: " + player1.getName());
+        System.out.println("Игрок " + player.getName() + " введите число");
+        int number = sc.nextInt();
+        player.setNumber(number);
+        System.out.println("Игрок " + player.getName() + " ввёл число " + player.getNumber());
+    }
 
     public int getHideNumber() {
         return hideNumber;
