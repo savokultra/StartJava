69efebfb9d281a8bf8ba14cf8943edff714d7028
Slava Ilyin <savok.ultrasound@gmail.com>
2023-06-27 20:29:24 +0300

    some pulp

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -8,35 +8,23 @@ public class GuessNumber {
     public GuessNumber() {
     }
 
-    public GuessNumber(int playerNumber) {
+    public GuessNumber(int playerNumber, int player1Number) {
         this.playerNumber = playerNumber;
+        this.player1Number = player1Number;
     }
+
+    /*public GuessNumber(int playerNumber,int player1Number) {
+        this.playerNumber = playerNumber;
+        this.player1Number = player1Number;
+    }*/
 
     public void showHideNumber() {
         System.out.println("hideNumber = " + hideNumber);
     }
-
-    public void showPlayersNumbers() {
-        System.out.println("playerNumber = " + playerNumber);
-        //System.out.println("player1Number = " + player1Number);
-    }
     
-    /*public static void playerInsert() { 
-        Player player = new Player();
-        System.out.println("Игрок " + player.getName() + " введите число");
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        player.setNumber(number);
-        GuessNumber playerNumber = new GuessNumber(player.getNumber());
-        System.out.println("Игрок " + player.getName() + " ввёл число " + player.getNumber());
+    public void playGame() {
         
-        Player player1 = new Player();
-        System.out.println("Игрок 1 " + player1.getName() + " введите число"); 
-        int number1 = sc.nextInt();
-        player1.setNumber(number1);
-        GuessNumber player1Number = new GuessNumber(number1);
-        System.out.println("Игрок 1 " + player1.getName() + " ввёл число " + player1.getNumber());
-    }*/
+    }
 
     public int getHideNumber() {
         return hideNumber;
--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -10,29 +10,24 @@ public class GuessNumberTest {
         Player player = new Player(name);
         System.out.println("имя игрока: " + player.getName());
         
-        /*System.out.println("Введите имя второго игрока: ");
+        System.out.println("Введите имя второго игрока: ");
         String name1 = sc.nextLine();
         Player player1 = new Player(name1);
-        System.out.println("имя второго игрока:: " + player1.getName());*/
+        System.out.println("имя второго игрока:: " + player1.getName());
         
         System.out.println("Игрок " + player.getName() + " введите число");
         int number = sc.nextInt();
         player.setNumber(number);
-        GuessNumber playerNumber = new GuessNumber(player.getNumber());
+        //GuessNumber playerNumber = new GuessNumber(player.getNumber());
         System.out.println("Игрок " + player.getName() + " ввёл число " + player.getNumber());
         
-        /*System.out.println("Игрок1 " + player1.getName() + " введите число");
+        System.out.println("Игрок1 " + player1.getName() + " введите число");
         sc.nextLine();
-        int number1 = sc.nextInt();
-        player1.setNumber(number1);
-        GuessNumber player1Number = new GuessNumber(player1.getNumber());
-        System.out.println("Игрок 1" + player1.getName() + " ввёл число " + player1.getNumber());*/
-        
+        number = sc.nextInt();
+        player1.setNumber(number);
+        System.out.println("Игрок 1 " + player1.getName() + " ввёл число " + player1.getNumber());
+        GuessNumber bets = new GuessNumber(player.getNumber(), player1.getNumber());
 
-        //GuessNumber.playerInsert();
-        //GuessNumber.play();
-
-        playerNumber.showPlayersNumbers();
-        playerNumber.showHideNumber();
+        bets.showHideNumber();
     }
 }
