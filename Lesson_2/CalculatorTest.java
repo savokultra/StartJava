6bce12008077aff239452e67cf21d89699936393
Slava <121571166+savokultra@users.noreply.github.com>
2023-06-27 15:00:55 +0300

    Update CalculatorTest.java

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -1,27 +1,38 @@
 import java.util.Scanner;
 
-public class CalculatorTest {
+public class GuessNumberTest {
 
     public static void main(String[] args) {
-        Calculator calcTest = new Calculator();
+
+        System.out.println("Введите имя игрока: ");
         Scanner sc = new Scanner(System.in);
-        String agree;
-        do {
-            System.out.println("Введите первое число: ");
-            int number = sc.nextInt();
-            calcTest.setNumber(number);
-            System.out.println("Введите знак математической операции: + - * / ^ ");
-            char sign = sc.next().charAt(0);
-            calcTest.setSign(sign);
-            System.out.println("Введите второе число: ");
-            int number1 = sc.nextInt();
-            calcTest.setNumber1(number1);
-            calcTest.calculate();
-            do {
-                System.out.println("Хотите продолжить вычисления? [yes/no]:");
-                sc.nextLine();
-                agree = sc.nextLine();
-            } while (!agree.equals("yes") && !agree.equals("no"));
-        } while (agree.equals("yes"));
+        String name = sc.nextLine();
+        Player player = new Player(name);
+        System.out.println("имя игрока: " + player.getName());
+        
+        /*System.out.println("Введите имя второго игрока: ");
+        String name1 = sc.nextLine();
+        Player player1 = new Player(name1);
+        System.out.println("имя второго игрока:: " + player1.getName());*/
+        
+        System.out.println("Игрок " + player.getName() + " введите число");
+        int number = sc.nextInt();
+        player.setNumber(number);
+        GuessNumber playerNumber = new GuessNumber(player.getNumber());
+        System.out.println("Игрок " + player.getName() + " ввёл число " + player.getNumber());
+        
+        /*System.out.println("Игрок1 " + player1.getName() + " введите число");
+        sc.nextLine();
+        int number1 = sc.nextInt();
+        player1.setNumber(number1);
+        GuessNumber player1Number = new GuessNumber(player1.getNumber());
+        System.out.println("Игрок 1" + player1.getName() + " ввёл число " + player1.getNumber());*/
+        
+
+        //GuessNumber.playerInsert();
+        //GuessNumber.play();
+
+        playerNumber.showPlayersNumbers();
+        playerNumber.showHideNumber();
     }
 }
