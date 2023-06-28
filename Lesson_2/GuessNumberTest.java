80551284619365a60d227f258a7108d45ae5dce1
Slava <121571166+savokultra@users.noreply.github.com>
2023-06-28 15:26:45 +0300

    AAAAAliluya!

--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -17,27 +17,13 @@ public class GuessNumberTest {
 
         GuessNumber players = new GuessNumber(player.getName(), player1.getName());
         
-        System.out.println("Первый игрок " + player.getName() + " введите число");
-        players.insertPlayerNumber();
-        System.out.println("Второй игрок " + player1.getName() + " введите число");
-        players.insertPlayer1Number();
-        
-        //System.out.println("playerNumber.getNumber() = " + playerNumber.getNumber());
-        //System.out.println("playerNumbers " + playerNumbers.getPlayerNumber() + " " + playerNumbers.getPlayer1Number());
+        do {
+            System.out.println("Первый игрок " + player.getName() + " введите число");
+            players.insertPlayerNumber();
+            System.out.println("Второй игрок " + player1.getName() + " введите число");
+            players.insertPlayer1Number();
+        } while (players.getPlayerNumber() != players.getHideNumber() && players.getPlayer1Number() != players.getHideNumber());
 
-        /*System.out.println("Первый игрок " + player.getName() + " введите число");
-        int number = sc.nextInt();
-        player.setNumber(number);
-        System.out.println("Первый игрок " + player.getName() + " ввёл число " + player.getNumber());
-
-        System.out.println("Второй игрок " + player1.getName() + " введите число");
-        int number1 = sc.nextInt();
-        player1.setNumber(number1);
-        System.out.println("Второй игрок " + player1.getName() + " ввёл число " + player1.getNumber());
-
-        GuessNumber bets = new GuessNumber(player.getNumber(), player1.getNumber());
-        
-        System.out.println("bets.playerNumber " + bets.getPlayerNumber());*/
-        //cycle.showHideNumber();
+        players.showHideNumber();
     }
 }
