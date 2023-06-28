10bb85e5440450124be7446f4f69affe5d96274d
Slava <121571166+savokultra@users.noreply.github.com>
2023-06-28 14:16:29 +0300

    Update GuessNumber.java

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -5,14 +5,9 @@ public class GuessNumber {
     private String player1Name;
     private int playerNumber;
     private int player1Number;
-    private boolean cycle;
     private int hideNumber = 1; //+ (int) (Math.random() * 100);
 
     public GuessNumber() {
-    }
-
-    public GuessNumber(boolean cycle) {
-        this.cycle = cycle;
     }
 
     public GuessNumber(String playerName, String player1Name) {
@@ -24,19 +19,46 @@ public class GuessNumber {
         this.playerNumber = playerNumber;
         this.player1Number = player1Number;
     }
-
-    public void showHideNumber() {
-        System.out.println("hideNumber = " + hideNumber);
+    
+    public int getPlayerNumber() {
+        return playerNumber;
     }
     
-    public boolean playGame() {
-        if (hideNumber != playerNumber && hideNumber != player1Number) {
-            System.out.println("Не один не угадал");
-        }
-        return false;
+    public int getPlayer1Number() {
+        return player1Number;
     }
-
-    public int getHideNumber() {
-        return hideNumber;
+    
+    public String getPlayerName() {
+        return playerName;
+    }
+    
+    public String getPlayer1Name() {
+        return player1Name;
+    }
+    
+    public void insertPlayerNumber() {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        Player playerNumber = new Player();
+        playerNumber.setNumber(number);
+        this.playerNumber = number;
+        System.out.println("playerNumber.getNumber() = " + playerNumber.getNumber());
+        System.out.println("playerName = " + playerName);
+        System.out.println("playerNumber = " + this.playerNumber);
+    }
+    
+    public void insertPlayer1Number() {
+        Scanner sc = new Scanner(System.in);
+        int number1 = sc.nextInt();
+        Player player1Number = new Player();
+        player1Number.setNumber(number1);
+        this.player1Number = number1;
+        System.out.println("player1Number.getNumber() = " + player1Number.getNumber());
+        System.out.println("player1Name = " + player1Name);
+        System.out.println("player1Number = " + this.player1Number);
+    }
+    
+    public void showHideNumber() {
+        System.out.println("hideNumber = " + hideNumber);
     }
 }
