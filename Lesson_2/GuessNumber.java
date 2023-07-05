64ecfdf7a10080be70f82589e206d0068ad52d10
Slava <121571166+savokultra@users.noreply.github.com>
2023-07-05 13:10:42 +0300

    Update GuessNumber.java

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -6,6 +6,7 @@ public class GuessNumber {
     private int playerNumber;
     private int player1Number;
     private int hideNumber;
+    String agree;
     Scanner sc = new Scanner(System.in);
 
     public GuessNumber(String playerName, String player1Name) {
@@ -32,33 +33,40 @@ public class GuessNumber {
     public void insertPlayerNumber() {
         do {
             startHideNumber();
-            System.out.println("\nПодсказка, искомое число = " + hideNumber);
-            System.out.println("\nПервый игрок " + playerName + " введите число");
-            int number = sc.nextInt();
-            this.playerNumber = number;
-            if (this.playerNumber < hideNumber) {
-                System.out.println("\nЧисло " + this.playerNumber + " меньше загаданного компьютером");
-            } else if (this.playerNumber > hideNumber) {
-                System.out.println("\nЧисло " + this.playerNumber + " больше загаданного компьютером");
-            } else {
-                System.out.println("\nВы угадали!\nЧисло " + this.playerNumber + " совпадает с" +
-                        "загаданным");
-            }
-            if (playerNumber == hideNumber) {
-                break;
-            }
-        
-            System.out.println("\nВторой игрок " + player1Name + " введите число");
-            int number1 = sc.nextInt();
-            this.player1Number = number1;
-            if (this.player1Number < hideNumber) {
-                System.out.println("\nЧисло " + this.player1Number + " меньше загаданного компьютером");
-            } else if (this.player1Number > hideNumber) {
-                System.out.println("\nЧисло " + this.player1Number + " больше загаданного компьютером");
-            } else {
-                System.out.println("\nВы победили!\nЧисло " + this.playerNumber + " совпадает с" +
-                        "загаданным");
-            }
-        } while (playerNumber != hideNumber && player1Number != hideNumber);
+            do {
+                System.out.println("\nПодсказка, искомое число = " + hideNumber);
+                System.out.println("\nПервый игрок " + playerName + " введите число");
+                int number = sc.nextInt();
+                this.playerNumber = number;
+                if (this.playerNumber < hideNumber) {
+                    System.out.println("\nЧисло " + this.playerNumber + " меньше загаданного компьютером");
+                } else if (this.playerNumber > hideNumber) {
+                    System.out.println("\nЧисло " + this.playerNumber + " больше загаданного компьютером");
+                } else {
+                    System.out.println("\nВы угадали!\nЧисло " + this.playerNumber + " совпадает с" +
+                            "загаданным");
+                }
+                if (playerNumber == hideNumber) {
+                    break;
+                }
+                
+                System.out.println("\nВторой игрок " + player1Name + " введите число");
+                int number1 = sc.nextInt();
+                this.player1Number = number1;
+                if (this.player1Number < hideNumber) {
+                    System.out.println("\nЧисло " + this.player1Number + " меньше загаданного компьютером");
+                } else if (this.player1Number > hideNumber) {
+                    System.out.println("\nЧисло " + this.player1Number + " больше загаданного компьютером");
+                } else {
+                    System.out.println("\nВы победили!\nЧисло " + this.playerNumber + " совпадает с" +
+                            "загаданным");
+                }
+            } while (playerNumber != hideNumber && player1Number != hideNumber);
+            do {
+                System.out.println("\nХотите продолжить игру [yes/no]:");
+                sc.nextLine();
+                agree = sc.nextLine();
+                } while (!agree.equals("yes") && !agree.equals("no"));
+        } while (agree.equals("yes"));
     }
 }
