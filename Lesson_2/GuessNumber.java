de374a60218b3482435ebe6e5e0568a8367c94d2
Slava <121571166+savokultra@users.noreply.github.com>
2023-07-05 16:53:11 +0300

    Update GuessNumber.java

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class GuessNumber {
     private String playerName;
     private String player1Name;
+    private String answer;
     private int playerNumber;
     private int player1Number;
     private int hideNumber;
-    String agree;
+    
     Scanner sc = new Scanner(System.in);
 
     public GuessNumber(String playerName, String player1Name) {
@@ -37,14 +38,14 @@ public class GuessNumber {
                 System.out.println("\nПодсказка, искомое число = " + hideNumber);
                 System.out.println("\nПервый игрок " + playerName + " введите число");
                 int number = sc.nextInt();
-                this.playerNumber = number;
-                if (this.playerNumber < hideNumber) {
-                    System.out.println("\nЧисло " + this.playerNumber + " меньше загаданного компьютером");
-                } else if (this.playerNumber > hideNumber) {
-                    System.out.println("\nЧисло " + this.playerNumber + " больше загаданного компьютером");
+                playerNumber = number;
+                if (playerNumber < hideNumber) {
+                    System.out.println("\nЧисло " + playerNumber + " меньше загаданного компьютером");
+                } else if (playerNumber > hideNumber) {
+                    System.out.println("\nЧисло " + playerNumber + " больше загаданного компьютером");
                 } else {
-                    System.out.println("\nВы угадали!\nЧисло " + this.playerNumber + " совпадает с" +
-                            "загаданным");
+                    System.out.println("\nВы угадали!\nЧисло " + playerNumber + " совпадает с" +
+                            " загаданным");
                 }
                 if (playerNumber == hideNumber) {
                     break;
@@ -52,21 +53,21 @@ public class GuessNumber {
                 
                 System.out.println("\nВторой игрок " + player1Name + " введите число");
                 int number1 = sc.nextInt();
-                this.player1Number = number1;
-                if (this.player1Number < hideNumber) {
-                    System.out.println("\nЧисло " + this.player1Number + " меньше загаданного компьютером");
-                } else if (this.player1Number > hideNumber) {
-                    System.out.println("\nЧисло " + this.player1Number + " больше загаданного компьютером");
+                player1Number = number1;
+                if (player1Number < hideNumber) {
+                    System.out.println("\nЧисло " + player1Number + " меньше загаданного компьютером");
+                } else if (player1Number > hideNumber) {
+                    System.out.println("\nЧисло " + player1Number + " больше загаданного компьютером");
                 } else {
-                    System.out.println("\nВы победили!\nЧисло " + this.playerNumber + " совпадает с" +
-                            "загаданным");
+                    System.out.println("\nВы победили!\nЧисло " + playerNumber + " совпадает с" +
+                            " загаданным");
                 }
             } while (playerNumber != hideNumber && player1Number != hideNumber);
             do {
                 System.out.println("\nХотите продолжить игру [yes/no]:");
                 sc.nextLine();
-                agree = sc.nextLine();
-            } while (!agree.equals("yes") && !agree.equals("no"));
-        } while (agree.equals("yes"));
+                answer = sc.nextLine();
+            } while (!answer.equals("yes") && !answer.equals("no"));
+        } while (answer.equals("yes"));
     }
 }
