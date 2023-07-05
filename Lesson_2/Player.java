09555b0608d2dfdd4125ec00c58df29aee748178
Slava <121571166+savokultra@users.noreply.github.com>
2023-07-05 16:54:17 +0300

    Update Player.java

--- Lesson_2/Player.java
+++ Lesson_2/Player.java
@@ -1,9 +1,14 @@
 public class Player {
     private String name;
     private int number;
-
+    
     public Player(String name) {
         this.name = name;
+    }
+
+    public Player(String name, int number) {
+        this.name = name;
+        this.number = number;
     }
 
     public String getName() {
