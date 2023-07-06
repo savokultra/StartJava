ddd52c6e0f4a876e0b7e006e5b4156f3c54b71e1
Slava <121571166+savokultra@users.noreply.github.com>
2023-07-06 12:57:35 +0300

    Update Player.java

--- Lesson_2/Player.java
+++ Lesson_2/Player.java
@@ -1,19 +1,17 @@
-import java.util.Scanner;
+public class Player {
+    private String player;
+    private int number;
+    
+    public Player(String player) {
+        this.player = player;
+    }
 
-public class GuessNumberTest {
+    public Player(String player, int number) {
+        this.player = player;
+        this.number = number;
+    }
 
-    public static void main(String[] args) {
-        System.out.println("Введите имя первого игрока: ");
-        Scanner sc = new Scanner(System.in);
-        Player player = new Player(sc.nextLine());
-
-        System.out.println("\nВведите имя второго игрока: ");
-        Player player1 = new Player(sc.nextLine());
-        
-        GuessNumber players1 = new GuessNumber(player, player1);
-
-        players1.insertPlayerNumber();
-        
-        players1.showPlayer();
+    public String getPlayer() {
+        return player;
     }
 }
