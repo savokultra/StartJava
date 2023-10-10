b939b3b9707618192198f7e361de9b8761f86d1c
Slava <121571166+savokultra@users.noreply.github.com>
2023-10-10 13:00:06 +0300

    Update GuessNumber.java

--- src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
@@ -6,8 +6,8 @@ public class GuessNumber {
     private Player player;
     private Player player1;
     private int hideNumber;
-    private int playerAttempt = 0;
-    private int player1Attempt = 0;
+    private int playerAttempt;
+    private int player1Attempt;
     Scanner sc = new Scanner(System.in);
     
 
@@ -20,52 +20,56 @@ public class GuessNumber {
         do {
             generateHiddenNumber();
             System.out.println("Игра началась! У каждого игрока по 10 попыток.");
-            do {
-                System.out.println("\nПодсказка, искомое число = " + hideNumber);
-                if (playerAttempt < 10) {
-                    System.out.println("\nПервый игрок " + player.getName() + " введите число");
-                    player.setAttempt(playerAttempt);
-                    playerAttempt++;
-                    int number = sc.nextInt();
-                    player.setNumber(number);
-                    if (player.getNumber() < hideNumber) {
-                        System.out.println("\nЧисло " + player.getNumber() + " меньше загаданного компьютером");
-                    } else if (player.getNumber() > hideNumber) {
-                        System.out.println("\nЧисло " + player.getNumber() + " больше загаданного компьютером");
-                    } else {
-                        System.out.println("\nВы угадали!\nЧисло " + player.getNumber() + " совпадает с" +
-                                " загаданным");
-                        break;
-                    }
-                    if (playerAttempt > 9) {
-                        System.out.println("У " + player.getName() + " закончились попытки");
-                    }
-                }
-                
-                if (player1Attempt < 10) {
-                    System.out.println("\nВторой игрок " + player1.getName() + " введите число");
-                    player1.setAttempt(player1Attempt);
-                    player1Attempt++;
-                    int number = sc.nextInt();
-                    player1.setNumber(number);
-                    if (player1.getNumber() < hideNumber) {
-                        System.out.println("\nЧисло " + player1.getNumber() + " меньше загаданного компьютером");
-                    } else if (player1.getNumber() > hideNumber) {
-                        System.out.println("\nЧисло " + player1.getNumber() + " больше загаданного компьютером");
-                    } else {
-                        System.out.println("\nВы победили!\nЧисло " + player1.getNumber() + " совпадает с" +
-                                " загаданным");
-                        break;
-                    }
-                    if (player1Attempt > 9) {
-                        System.out.println("У " + player1.getName() + " закончились попытки");
-                    }
-                }
-            } while (playerAttempt < 10 || player1Attempt < 10);
-        } while ((player.getNumber() != hideNumber && player1.getNumber() != hideNumber) && (playerAttempt < 10 || player1Attempt < 10));
+            game();
+        } while (player.getNumber() != hideNumber && player1.getNumber() != hideNumber && (playerAttempt < 10 || player1Attempt < 10));
     }
 
     private void generateHiddenNumber() {
         hideNumber = 1 + (int) (Math.random() * 100);
+    }
+    
+    private void game() {
+        playerAttempt = 0;
+        player1Attempt = 0;
+        do {
+            System.out.println("\nПодсказка, искомое число = " + hideNumber);
+            if (playerAttempt < 10) {
+                System.out.println("\nПервый игрок " + player.getName() + " введите число");
+                player.setAttempt(playerAttempt);
+                playerAttempt++;
+                int number = sc.nextInt();
+                player.setNumber(number);
+                if (player.getNumber() < hideNumber) {
+                    System.out.println("\nЧисло " + player.getNumber() + " меньше загаданного компьютером");
+                } else if (player.getNumber() > hideNumber) {
+                    System.out.println("\nЧисло " + player.getNumber() + " больше загаданного компьютером");
+                } else {
+                    System.out.println("\nИгрок " + player.getName() + " угадал число " + player.getNumber() + " с " + playerAttempt + " попытки");
+                    break;
+                }
+                if (playerAttempt > 9) {
+                    System.out.println("У " + player.getName() + " закончились попытки");
+                }
+            }
+                
+            if (player1Attempt < 10) {
+                System.out.println("\nВторой игрок " + player1.getName() + " введите число");
+                player1.setAttempt(player1Attempt);
+                player1Attempt++;
+                int number = sc.nextInt();
+                player1.setNumber(number);
+                if (player1.getNumber() < hideNumber) {
+                    System.out.println("\nЧисло " + player1.getNumber() + " меньше загаданного компьютером");
+                } else if (player1.getNumber() > hideNumber) {
+                    System.out.println("\nЧисло " + player1.getNumber() + " больше загаданного компьютером");
+                } else {
+                    System.out.println("\nИгрок " + player1.getName() + " угадал число " + player1.getNumber() + " с " + player1Attempt + " попытки");
+                    break;
+                }
+                if (player1Attempt > 9) {
+                    System.out.println("У " + player1.getName() + " закончились попытки");
+                }
+            }
+        } while (playerAttempt < 10 || player1Attempt < 10);
     }
 }
