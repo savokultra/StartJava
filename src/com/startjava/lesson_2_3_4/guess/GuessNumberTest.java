54741f747844a88d1dc7641c58d1c581d1b0c5fe
Slava <121571166+savokultra@users.noreply.github.com>
2023-10-16 15:04:36 +0300

    Update GuessNumberTest.java

--- src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
@@ -1,9 +1,11 @@
 package com.startjava.lesson_2_3_4.guess;
 
+import java.util.Scanner;
+
 public class GuessNumberTest {
 
     public static void main(String[] args) {
-        String answer = "";
+        String answer = "yes";
         System.out.println("Введите имя первого игрока: ");
         Scanner sc = new Scanner(System.in);
         String player = sc.nextLine();
@@ -13,16 +15,12 @@ public class GuessNumberTest {
         GuessNumber players = new GuessNumber(player, player1);
 
         do {
-            players.start();
-            if (!answer.equals("yes") && !answer.equals("no")) {
-                System.out.println("\nХотите продолжить игру [yes/no]:");
-                answer = sc.nextLine();
+            if (answer.equals("yes")) {
+                players.start();
+                players.arraysFill();
             }
-            /*do {
-                System.out.println("\nХотите продолжить игру [yes/no]:");
-                answer = sc.nextLine();
-            } while (!answer.equals("yes") && !answer.equals("no"));*/
-            players.arraysFill();
-        } while (answer.equals("yes"));
+            System.out.println("\nХотите продолжить игру [yes/no]:");
+            answer = sc.nextLine();
+        } while (!answer.equals("no"));
     }
 }
