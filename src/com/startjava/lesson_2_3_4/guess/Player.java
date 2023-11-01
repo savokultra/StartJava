3860d55d7c46ef205a6f7fd20fc8608e609d97bd
Slava <121571166+savokultra@users.noreply.github.com>
2023-11-01 10:10:22 +0300

    Update Player.java

--- src/com/startjava/lesson_2_3_4/guess/Player.java
+++ src/com/startjava/lesson_2_3_4/guess/Player.java
@@ -1,8 +1,11 @@
 package com.startjava.lesson_2_3_4.guess;
 
+import java.util.Arrays;
+
 public class Player {
     private String name;
-    private int number;
+    private int[] numbers = new int[10];
+    private int attempt;
 
     public Player(String name) {
         this.name = name;
@@ -13,10 +16,23 @@ public class Player {
     }
 
     public int getNumber() {
-        return number;
+        return numbers[attempt - 1];
     }
 
-    public void setNumber(int number) {
-        this.number = number;
+    public void addNumber(int number) {
+        numbers[attempt++] = number;
+    }
+    
+    public int getAttemptNumber() {
+        return attempt;
+    }
+    
+    public int[] getAllNumbers() {
+        return Arrays.copyOf(numbers, attempt);
+    }
+
+    public void clear() {
+        Arrays.fill(numbers, 0, attempt, 0);
+        attempt = 0;
     }
 }
