e1324a90a17764423bb0c63b463a9ffc0f0896cc
Slava_Ilyin <savok.ultrasound@gmail.com>
2023-11-18 01:00:30 +0300

    add final static fields in GuessNumber

--- src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
@@ -4,14 +4,13 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class GuessNumber {
-    private final Player[] players = new Player[3];
-
-    private int hiddenNumber;
-    private final Scanner sc = new Scanner(System.in);
+    private static final Player[] PLAYERS = new Player[3];
+    private static final Scanner SC = new Scanner(System.in);
+    private static final int hiddenNumber = 1 + (int) (Math.random() * 100);
 
     public GuessNumber(String[] names) {
-        for (int i = 0; i < players.length; i++) {
-            players[i] = new Player(names[i]);
+        for (int i = 0; i < PLAYERS.length; i++) {
+            PLAYERS[i] = new Player(names[i]);
         }
     }
 
@@ -23,15 +22,15 @@ public class GuessNumber {
     private void shufflePlayers() {
         Random rand = new Random();
         Player temp;
-        for (int i = players.length - 1; i > 0; i--) {
+        for (int i = PLAYERS.length - 1; i > 0; i--) {
             int j = rand.nextInt(i + 1);
-            temp = players[i];
-            players[i] = players[j];
-            players[j] = temp;
+            temp = PLAYERS[i];
+            PLAYERS[i] = PLAYERS[j];
+            PLAYERS[j] = temp;
         }
         System.out.println("\nЖребий брошен, игроки ходят в следующем порядке: ");
-        for(int i = 0; i <= players.length - 1; i++) {
-            System.out.println((i + 1) + "м угадывает - " + players[i].getName());
+        for(int i = 0; i <= PLAYERS.length - 1; i++) {
+            System.out.println((i + 1) + "м угадывает - " + PLAYERS[i].getName());
         }
     }
 
@@ -42,14 +41,14 @@ public class GuessNumber {
             boolean b = true;
             while (b) {
                 System.out.println("\nПодсказка, искомое число = " + hiddenNumber);
-                for (Player x : players) {
+                for (Player x : PLAYERS) {
                     if (isGuessed(x)) {
                         b = false;
                         break;
                     }
                 }
             }
-            showPlayerNumbers(players);
+            showPlayerNumbers();
             clear();
         }
         showWins();
@@ -80,20 +79,20 @@ public class GuessNumber {
         int number;
         do {
             System.out.print("\nИгрок " + player.getName() + " введите число от 1 до 100: ");
-            number = sc.nextInt();
+            number = SC.nextInt();
         } while (player.addNumber(number));
     }
 
     private boolean checkAttempt(Player player) {
         if (player.getAttempt() > 9) {
             System.out.println("У " + player.getName() + " закончились попытки");
-            return players[2].getAttempt() > 9;
+            return PLAYERS[2].getAttempt() > 9;
         }
         return false;
     }
 
-    private void showPlayerNumbers(Player[] players) {
-        for (Player player : players) {
+    private void showPlayerNumbers() {
+        for (Player player : GuessNumber.PLAYERS) {
             System.out.print("\nИгрок " + player.getName() + " загадал числа: ");
             int[] numbers = player.getAllNumbers();
             for (int number : numbers) {
@@ -104,20 +103,20 @@ public class GuessNumber {
     }
 
     private void showWins() {
-        for (Player player : players) {
+        for (Player player : PLAYERS) {
             System.out.println("Игрок " + player.getName() + " угадал загаданное число " + player.getWins() +
                     (player.getWins() > 1 ? " раза" : " раз"));
         }
     }
 
     private void clear() {
-        for (Player player : players) {
+        for (Player player : PLAYERS) {
             player.clear();
         }
     }
 
     private void clearWins() {
-        for (Player player : players) {
+        for (Player player : PLAYERS) {
             player.clearWins();
         }
     }
