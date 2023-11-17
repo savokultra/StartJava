45e8f2a7ed57294b676bc887b396239d483f13bb
Slava_Ilyin <savok.ultrasound@gmail.com>
2023-11-18 01:10:22 +0300

    update insertNames

--- src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
@@ -1,5 +1,6 @@
 package com.startjava.lesson_2_3_4.guess;
 
+import javax.print.attribute.standard.MediaSize;
 import java.util.Scanner;
 
 public class GuessNumberTest {
@@ -7,16 +8,16 @@ final static String[] NAMES = new String[3];
 final static Scanner SC = new Scanner(System.in);
 
     public static void main(String[] args) {
-        insertNames();
-        GuessNumber game = new GuessNumber(NAMES);
+        GuessNumber game = new GuessNumber(inputNames());
         gameContinue(game);
     }
 
-    private static void insertNames() {
+    private static String[] inputNames() {
         for (int i = 0; i < GuessNumberTest.NAMES.length; i++) {
             System.out.println("Введите имя " + (i + 1) + " игрока: ");
             GuessNumberTest.NAMES[i] = SC.nextLine();
         }
+        return NAMES;
     }
 
     private static void gameContinue(GuessNumber game) {
