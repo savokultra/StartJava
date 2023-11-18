b8a42cc142bb2de8c2307e6dd16a1a41b8e3b3d8
Slava_Ilyin <savok.ultrasound@gmail.com>
2023-11-18 14:58:34 +0300

    update GuessNumberTest

--- src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
@@ -1,6 +1,5 @@
 package com.startjava.lesson_2_3_4.guess;
 
-import javax.print.attribute.standard.MediaSize;
 import java.util.Scanner;
 
 public class GuessNumberTest {
@@ -8,19 +7,11 @@ final static String[] NAMES = new String[3];
 final static Scanner SC = new Scanner(System.in);
 
     public static void main(String[] args) {
+        playGame();
+    }
+
+    private static void playGame() {
         GuessNumber game = new GuessNumber(inputNames());
-        gameContinue(game);
-    }
-
-    private static String[] inputNames() {
-        for (int i = 0; i < GuessNumberTest.NAMES.length; i++) {
-            System.out.println("Введите имя " + (i + 1) + " игрока: ");
-            GuessNumberTest.NAMES[i] = SC.nextLine();
-        }
-        return NAMES;
-    }
-
-    private static void gameContinue(GuessNumber game) {
         String answer = "yes";
         do {
             if (answer.equals("yes")) {
@@ -29,5 +20,13 @@ final static Scanner SC = new Scanner(System.in);
             System.out.println("\nХотите продолжить игру [yes/no]:");
             answer = SC.nextLine();
         } while (!answer.equals("no"));
+    }
+
+    private static String[] inputNames() {
+        for (int i = 0; i < GuessNumberTest.NAMES.length; i++) {
+            System.out.println("Введите имя " + (i + 1) + " игрока: ");
+            GuessNumberTest.NAMES[i] = SC.nextLine();
+        }
+        return NAMES;
     }
 }
