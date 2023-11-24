cc0b046cb0fc723d42a33cce42fb699e74bcb0c1
Slava_Ilyin <savok.ultrasound@gmail.com>
2023-11-24 22:52:02 +0300

    add PLAYERS_QUANTITY

--- src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
@@ -3,14 +3,14 @@ package com.startjava.lesson_2_3_4.guess;
 import java.util.Scanner;
 
 public class GuessNumberTest {
-final static String[] NAMES = new String[3];
-final static Scanner SC = new Scanner(System.in);
+private static final int PLAYERS_QUANTITY = 3;
 
     public static void main(String[] args) {
         playGame();
     }
 
     private static void playGame() {
+        Scanner sc = new Scanner(System.in);
         GuessNumber game = new GuessNumber(inputNames());
         String answer = "yes";
         do {
@@ -18,14 +18,16 @@ final static Scanner SC = new Scanner(System.in);
                 game.start();
             }
             System.out.println("\nХотите продолжить игру [yes/no]:");
-            answer = SC.nextLine();
+            answer = sc.nextLine();
         } while (!answer.equals("no"));
     }
 
     private static String[] inputNames() {
-        for (int i = 0; i < GuessNumberTest.NAMES.length; i++) {
+        String[] NAMES = new String[PLAYERS_QUANTITY];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < NAMES.length; i++) {
             System.out.println("Введите имя " + (i + 1) + " игрока: ");
-            GuessNumberTest.NAMES[i] = SC.nextLine();
+            NAMES[i] = sc.nextLine();
         }
         return NAMES;
     }
